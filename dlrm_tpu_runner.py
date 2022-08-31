54ac69ca43d77797242726c8850f78fb923146d5
darisoy <darisoy@google.com>
2022-08-31 23:55:58 +0000

    Remove options from the unused --tpu-cores arg but keep the arg as tests still pass it in

--- dlrm_tpu_runner.py
+++ dlrm_tpu_runner.py
@@ -8,9 +8,7 @@ from dlrm_s_pytorch import main, parse_args
 
 if __name__ == '__main__':
     pre_spawn_parser = argparse.ArgumentParser()
-    pre_spawn_parser.add_argument(
-        "--tpu-cores", type=int, default=8, choices=[1, 8]
-    )
+    pre_spawn_parser.add_argument("--tpu-cores", type=int, default=8)
     pre_spawn_flags, _ = pre_spawn_parser.parse_known_args()
     pjrt.run_multiprocess(main)
     # xmp.spawn(main, args=(), nprocs=pre_spawn_flags.tpu_cores)
